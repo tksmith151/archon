e737e3ef29b4b56b85065e72b54f3bcb0c7ab569
Trevor Smith <tksmith151@gmail.com>
2024-09-02 17:16:59 -0700

    Added partitioning and formatting

--- prepare.py
+++ prepare.py
@@ -6,36 +6,46 @@ def list_disks():
     lsblk.run()
     disks: List[str] = []
     output: List[str] = []
-    print(lsblk.stdout)
-    print(lsblk.stderr)
     for device in json.loads(lsblk.stdout).get("blockdevices"):
         device_type = device.get("type")
         if device_type == "disk":
             device_name = device.get("name")
             device_size = device.get("size")
             disks.append(f"{device_name} - {device_size}")
-            output.append(device_name)
+            output.append(f"/dev/{device_name}")
     if not disks:
         raise Exception("No disks found")
     display_lines(disks)
     return output
 
-
-def partition():
+def partition(disk_name: str):
     efi = Command()
     efi.add("Create efi partition")
-    efi.set("")
+    efi.set(f"parted -s {disk_name} mklabel gpt mkpart primary ext4 0% 2GiB set 1 boot on")
     efi.show_summary()
 
-    root = Command()
-    root.add("Create root partition")
-    root.set("")
-    root.show_summary()
+    btrfs = Command()
+    btrfs.add("Create btrfs partition")
+    btrfs.set(f"parted -s {disk_name} mklabel msdos mkpart primary ext4 0% 2GiB set 1 boot on")
+    btrfs.show_summary()
 
+
+def format(disk_name: str):
+    efi = Command()
+    efi.set(f"mkfs.fat -F 32 {disk_name}p1")
+    efi.show_summary()
+
+    btrfs = Command()
+    btrfs.set(f"mkfs.btrfs {disk_name}p2")
+    btrfs.show_summary()
 
 def main():
     disks = list_disks()
-    # partition()
+    if len(disks) > 1:
+        raise Exception("Too many disks")
+    disk = disks[0]
+    partition(disk)
+    format(disk)
 
 if __name__ == "__main__":
     main()
