2ef858e09cfdcb1b7d0c79f47cc5765130163fb3
Trevor Smith <tksmith151@gmail.com>
2024-09-06 12:11:56 -0700

    Create needed subvolumes

--- prepare.py
+++ prepare.py
@@ -1,17 +1,18 @@
 from utility import *
 
 def partition(disk_name: str):
-    gpt = Command(f"sgdisk -og {disk_name}")
+    gpt = Command(f"sgdisk -z {disk_name}")
     gpt.comment("Clear all partitions from disk")
     gpt.comment("Only do this if you are installing for the first time")
+    gpt.comment("This will destroy data")
     gpt.confirm()
 
-    efi = Command(f'sgdisk -n 1:0:+512MiB -c 1:"EFI System Partition" -t 1:ef00 {disk_name}')
+    efi = Command(f'sgdisk -n 1:0:+512MiB -c 1:"EFI-System-Partition" -t 1:ef00 {disk_name}')
     efi.comment("Create efi partition")
     efi.comment("Creates a 512MiB Partition at the first avaialable location")
     efi.confirm()
 
-    btrfs = Command(f'sgdisk -n 2:0:0 -c 2:"BTRFS Partition" -t 2:8300 {disk_name}')
+    btrfs = Command(f'sgdisk -n 2:0:0 -c 2:"BTRFS-Partition" -t 2:8300 {disk_name}')
     btrfs.comment("Create btrfs partition")
     btrfs.comment("Creates a btrfs partition with the rest of the disk")
     btrfs.confirm()
@@ -41,10 +42,7 @@ def subvolume(btrfs_partition: str):
     umount.run()
 
 def main():
-    disks = list_disks()
-    if len(disks) > 1:
-        raise Exception("Too many disks")
-    disk = disks[0]
+    disk = select_disk()
     partition(disk)
     efi_partition, btrfs_partition = list_partitions(disk)
     format(efi_partition, btrfs_partition)
