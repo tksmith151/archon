396faac61ab9d572469a5659490a57a67d680d8a
Trevor Smith <tksmith151@gmail.com>
2024-09-06 11:48:08 -0700

    Added subvolume utility

--- prepare.py
+++ prepare.py
@@ -1,86 +1,43 @@
 from utility import *
 
-def list_disks():
-    lsblk = Command()
-    lsblk.set("lsblk --json")
-    lsblk.run()
-    disks: List[str] = []
-    output: List[str] = []
-    for device in json.loads(lsblk.stdout).get("blockdevices"):
-        device_type = device.get("type")
-        if device_type == "disk":
-            device_name = device.get("name")
-            device_size = device.get("size")
-            disks.append(f"{device_name} - {device_size}")
-            output.append(f"/dev/{device_name}")
-    if not disks:
-        raise Exception("No disks found")
-    return output
-
 def partition(disk_name: str):
-    gpt = Command()
-    gpt.add("Clear all partitions from disk")
-    gpt.add("Only do this if you are installing for the first time")
-    gpt.set(f"sgdisk -og {disk_name}")
+    gpt = Command(f"sgdisk -og {disk_name}")
+    gpt.comment("Clear all partitions from disk")
+    gpt.comment("Only do this if you are installing for the first time")
     gpt.confirm()
 
-    efi = Command()
-    efi.add("Create efi partition")
-    efi.add("Creates a 2GiB Partition at the first avaialable location")
-    efi.set(f'sgdisk -n 1:0:+2GiB -c 1:"EFI System Partition" -t 1:ef00 {disk_name}')
+    efi = Command(f'sgdisk -n 1:0:+512MiB -c 1:"EFI System Partition" -t 1:ef00 {disk_name}')
+    efi.comment("Create efi partition")
+    efi.comment("Creates a 512MiB Partition at the first avaialable location")
     efi.confirm()
 
-    btrfs = Command()
-    btrfs.add("Create btrfs partition")
-    btrfs.add("Creates a btrfs partition with the rest of the disk")
-    btrfs.set(f'sgdisk -n 2:0:0 -c 2:"BTRFS Partition" -t 2:8300 {disk_name}')
+    btrfs = Command(f'sgdisk -n 2:0:0 -c 2:"BTRFS Partition" -t 2:8300 {disk_name}')
+    btrfs.comment("Create btrfs partition")
+    btrfs.comment("Creates a btrfs partition with the rest of the disk")
     btrfs.confirm()
 
-def list_partitions(disk_name):
-    disk_name
-    lsblk = Command()
-    lsblk.set(f"lsblk --json {disk_name}")
-    lsblk.run()
-    efi_partition = None
-    btrfs_partition = None
-    drive = json.loads(lsblk.stdout).get("blockdevices")[0]
-    partitions = drive["children"]
-    if len(partitions) != 2:
-        raise Exception("Incorrect number of partitions")
-    for partition in drive["children"]:
-        partition_type = partition["type"]
-        if partition_type != "part":
-            continue
-        partition_name = partition["name"]
-        if partition["size"] == "2G":
-            efi_partition = f"/dev/{partition_name}"
-        else:
-            btrfs_partition = f"/dev/{partition_name}"
-    return efi_partition, btrfs_partition
-
 def format(efi_partition, btrfs_partition):
-    efi = Command()
-    efi.add("Format the efi partition")
-    efi.set(f"mkfs.fat -F 32 {efi_partition}")
+    efi = Command(f"mkfs.fat -F 32 {efi_partition}")
+    efi.comment("Format the efi partition")
     efi.confirm()
 
-    btrfs = Command()
-    btrfs.add("Format the btrfs partition")
-    btrfs.set(f"mkfs.btrfs {btrfs_partition}")
+    btrfs = Command(f"mkfs.btrfs {btrfs_partition}")
+    btrfs.comment("Format the btrfs partition")
     btrfs.confirm() 
 
 def subvolume(btrfs_partition: str):
-    mount = Command()
-    mount.set(f"mount {btrfs_partition} /mnt")
+    mount = Command(f"mount {btrfs_partition} /mnt")
     mount.run()
 
-    root = Command()
-    root.add("Add root btrfs subvolume")
-    root.set("btrfs subvolume create /mnt/@")
+    root = Command("btrfs subvolume create /mnt/@arch-root")
+    root.comment("Add arch btrfs subvolume")
     root.confirm()
 
-    umount = Command()
-    umount.set(f"umount /mnt")
+    root = Command("btrfs subvolume create /mnt/@my")
+    root.comment("Add my btrfs subvolume")
+    root.confirm()
+
+    umount = Command(f"umount /mnt")
     umount.run()
 
 def main():
--- utility.py
+++ utility.py
@@ -8,19 +8,13 @@ def display_lines(lines: List[str]):
         print(line)
 
 class Command:
-    def __init__(self) -> None:
+    def __init__(self, string: str = None) -> None:
+        self._string: str = string
         self._description: List[str] = []
-        self._string: str = None
         self._result = None
 
-    def add(self, description: str):
+    def comment(self, description: str):
         self._description.append(f"# {description}")
-
-    def set(self, string: str):
-        if not self._string:
-            self._string = string
-            return
-        raise Exception("Cannot set a command string twice")
 
     def show_summary(self):
         display_lines(self._description)
@@ -58,3 +52,78 @@ class Command:
     def stderr(self):
         output: str = self._result.stderr
         return output
+
+def select_disk():
+    disks = list_disks()
+
+def list_disks():
+    lsblk = Command("lsblk --json")
+    lsblk.run()
+    disks: List[str] = []
+    output: List[str] = []
+    for device in json.loads(lsblk.stdout).get("blockdevices"):
+        device_type = device.get("type")
+        if device_type == "disk":
+            device_name = device.get("name")
+            device_size = device.get("size")
+            disks.append(f"{device_name} - {device_size}")
+            output.append(f"/dev/{device_name}")
+    if not disks:
+        raise Exception("No disks found")
+    return output
+
+def select_disk():
+    output = None
+    while output is None:
+        disks = list_disks()
+        print("Disks:")
+        for index, disk in enumerate(disks):
+            print(index, disk)
+        disk_index = input("Select disk by index")
+        try:
+            disk = disks[disk_index]
+            output = disk
+        except:
+            print("Invalid disk index")
+        print()
+    return output
+
+
+def list_partitions(disk_name: str):
+    lsblk = Command(f"lsblk --json {disk_name}")
+    lsblk.run()
+    efi_partition = None
+    btrfs_partition = None
+    drive = json.loads(lsblk.stdout).get("blockdevices")[0]
+    partitions = drive["children"]
+    if len(partitions) != 2:
+        raise Exception("Incorrect number of partitions")
+    for partition in drive["children"]:
+        partition_type = partition["type"]
+        if partition_type != "part":
+            continue
+        partition_name = partition["name"]
+        if partition["size"] == "512M":
+            efi_partition = f"/dev/{partition_name}"
+        else:
+            btrfs_partition = f"/dev/{partition_name}"
+    return efi_partition, btrfs_partition
+
+def list_subvolumes(btrfs_partition):
+    mount = Command(f"mount {btrfs_partition} /mnt")
+    mount.run()
+
+    btrfs = Command(f"btrfs subvolume list -a /mnt")
+    btrfs.run()
+    output = btrfs.stdout
+    
+    paths = set()
+    lines = output.split("\n")
+    for line in lines:
+        fields = line.split(" ")
+        path = fields[-1]
+        paths.add(path)
+
+    umount = Command(f"umount /mnt")
+    umount.run()
+
