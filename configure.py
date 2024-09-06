69fee41a3a3a50f69f977c713116fdd24177d59e
Trevor Smith <tksmith151@gmail.com>
2024-09-06 13:56:52 -0700

    Comment

--- configure.py
+++ configure.py
@@ -2,6 +2,7 @@ from utility import *
 
 def main():
     copy_file("/tmp/fstab", "/mnt/etc/fstab")
+    # Needed for wireless cards
     copy_file("/lib/firmware/regulatory.db", "/mnt/lib/firmware/regulatory.db")
     copy_file("/lib/firmware/regulatory.db.p7s", "/mnt/lib/firmware/regulatory.db.p7s")
 
