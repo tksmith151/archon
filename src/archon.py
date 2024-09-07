a6197e354039dafaa6078f0e5cb10242d95d8485
Trevor Smith <tksmith151@gmail.com>
2024-09-06 22:38:27 -0700

    Added arg parsing to main

--- src/archon.py
+++ src/archon.py
@@ -1,5 +1,26 @@
+import argparse
+
+def parse_args():
+    root_parser = argparse.ArgumentParser(prog="calc")
+    root_subparsers = root_parser.add_subparsers(
+        title="subcommands", help="Arch Linux Operations"
+    )
+
+    install_parser = root_subparsers.add_parser("install")
+    install_parser.add_argument(
+        dest="step",
+        type=str,
+        choices=['partition', 'format'],
+        metavar="STEP",
+        help="string indicate the step to run",
+    )
+
+    return root_parser.parse_args()
+
+
 def main():
-    print("Launched Archon")
+    args = parse_args()
+    print(args.step)
 
 if __name__ == "__main__":
     main()
