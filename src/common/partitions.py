93ffd0c99f25caa99fd8aadec6651925a065a93f
Trevor Smith <tksmith151@gmail.com>
2024-09-08 00:42:40 -0700

    Testing

--- src/common/partitions.py
+++ src/common/partitions.py
@@ -1,14 +1,17 @@
 from namespace.common import *
 
 def get_partitions(disk_name: str):
-    sfdisk = Command(f"sfdisk --json {disk_name}", quiet=True)
-    table = json.loads(sfdisk.stdout)["partitiontable"]
-    partition_data: List[Dict] = table["partitions"]
-    partitions: Dict[str, str] = {}
-    for partition in partition_data:
-        name = partition.get("name", None)
-        if name is None:
-            continue
-        node = partition["node"]
-        partitions[name] = node
-    return partitions
+    try:
+        sfdisk = Command(f"sfdisk --json {disk_name}", quiet=True)
+        table = json.loads(sfdisk.stdout)["partitiontable"]
+        partition_data: List[Dict] = table["partitions"]
+        partitions: Dict[str, str] = {}
+        for partition in partition_data:
+            name = partition.get("name", None)
+            if name is None:
+                continue
+            node = partition["node"]
+            partitions[name] = node
+        return partitions
+    except:
+        return None
