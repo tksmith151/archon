ba45e519a839c5af3525427cebf309f15cf30fa2
Trevor Smith <tksmith151@gmail.com>
2024-09-07 22:17:46 -0700

    Added more bootstrap steps

--- src/install/bootstrap.py
+++ src/install/bootstrap.py
@@ -1,11 +1,18 @@
 from namespace.standard import *
 
 def bootstrap():
+    # TODO: Check for mounts
     Command(f"pacstrap -K /mnt base").run()
+    # Copy fstab
+    copy_file("/tmp/fstab", "/mnt/etc/fstab")
+    # Needed for wireless cards
+    copy_file("/lib/firmware/regulatory.db", "/mnt/lib/firmware/regulatory.db")
+    copy_file("/lib/firmware/regulatory.db.p7s", "/mnt/lib/firmware/regulatory.db.p7s")
 
 
 def main():
     bootstrap()
+
 
 if __name__ == "__main__":
     main()
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,3 +1,4 @@
 from src.util.command import Command
 from src.util.confirm import confirm
+from src.util.copy_file import copy_file
 from src.util.write_file import write_file
