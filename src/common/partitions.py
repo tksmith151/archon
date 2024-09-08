ca600a09732fb47d9ff446db342d2a4aa8059ea8
Trevor Smith <tksmith151@gmail.com>
2024-09-07 17:08:23 -0700

    Testing

--- src/common/partitions.py
+++ src/common/partitions.py
@@ -5,6 +5,7 @@ def get_partitions(disk_name: str):
     table = json.loads(sfdisk.stdout)["partitiontable"]
     partition_data = table["partitions"]
     partitions: Dict[str, str] = {}
+    print(partition_data)
     for partition in partition_data:
         name = partition["name"]
         node = partition["node"]
